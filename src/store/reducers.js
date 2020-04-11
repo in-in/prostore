58ad4e843577bdc98726c3d94fb7c1bbabd4bd48
in-in <dmin@tuta.io>
2020-04-11 19:14:57 +0300

    feat(redux): add reducers

---
+++ src/store/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+	'books': [],
+};
+
+export const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'BOOKS_LOADED':
+			return {
+				'books': action.payload,
+			};
+		default:
+			return state;
+	}
+};
