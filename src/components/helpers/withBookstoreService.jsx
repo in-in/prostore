0da29d55f25059c9933abe20023a7ac01b1cf747
in-in <dmin@tuta.io>
2020-04-11 16:03:32 +0300

    feat(withbookstoreservice): add withBookstoreService hoc

---
+++ src/components/helpers/withBookstoreService.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BookstoreServiceConsumer } from '../../services/bookstore-context';
+
+export const withBookstoreService = () => (
+	Wrapped,
+) => (props) => (
+	<BookstoreServiceConsumer>
+		{
+			(bookstoreService) => (
+				<Wrapped {...props} bookstoreService={bookstoreService} />
+			)
+		}
+	</BookstoreServiceConsumer>
+);
