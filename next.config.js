f39e4ed28698e3af248dbdd773fcd8b05bba03a5
in-in <dmin@tuta.io>
2020-04-15 19:08:10 +0300

    fix(alias): return the base aliases

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-param-reassign */
 const { join } = require('path');
-const withSass = require('@zeit/next-sass');
-const sass = require('sass');
 
 const config = {
 	'poweredByHeader': false,
 	webpack(cfg) {
 		cfg.resolve.alias = {
+			...cfg.resolve.alias,
 			'~': join(__dirname, 'src'),
 			'@store': join(__dirname, 'src', 'store'),
 			'@service': join(__dirname, 'src', 'services'),
@@ -16,18 +15,4 @@ const config = {
 	},
 };
 
-const sassConfig = {
-	'cssModules': true,
-	'sassLoaderOptions': {
-		'implementation': sass,
-	},
-};
-
-const cssConfig = {
-	'cssModules': true,
-	'cssLoaderOptions': {
-		'localIdentName': '[local]_[hash:base64:5]',
-	},
-};
-
-module.exports = withSass({ ...config, ...cssConfig, ...sassConfig });
+module.exports = { ...config };
