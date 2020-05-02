93f5af9e5dce2af8ab7e1fd096ac0dfc92c004d2
in-in <dmin@tuta.io>
2020-05-02 18:46:32 +0300

    refactor(table): add store

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,17 +1,14 @@
-// import PropTypes from 'prop-types';
-// import Link from 'next/link';
-// import Cart from '../../assets/icons/cart.svg';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { TableButton } from '../TableButton';
 import Close from '../../assets/icons/close.svg';
 import CartMinus from '../../assets/icons/cart_minus.svg';
 import CartPlus from '../../assets/icons/cart_plus.svg';
 import st from './style.module.scss';
 
-export const Table = () =>
-// const {
-// 	// author, price, title,
-// } = book;
-
+const BaseTable = ({
+	items, total, onIncrease, onDecrease, onDelete,
+}) =>
 	(
 		<table className={st.table}>
 			<caption className={st.table_caption}>Your Order</caption>
@@ -20,60 +17,73 @@ export const Table = () =>
 					<th className={st.table_head}>#</th>
 					<th className={st.table_head}>Item</th>
 					<th className={st.table_head}>Count</th>
-					<th className={st.table_head}>Price</th>
+					<th className={st.table_head}>Total</th>
 					<th className={st.table_head}>Action</th>
 				</tr>
 			</thead>
 			<tbody className={st.table_body}>
-				<tr>
-					<td className={st.table_item}>1</td>
-					<td className={st.table_itemName}>Production-Ready Microservices</td>
-					<td className={st.table_item}>2</td>
-					<td className={st.table_item}>$40</td>
-					<td className={st.table_buttonGroup}>
-						<TableButton icon={<CartMinus />} />
-						<TableButton icon={<CartPlus />} />
-						<TableButton icon={<Close />} />
-					</td>
-				</tr>
-				<tr>
-					<td className={st.table_item}>1</td>
-					<td className={st.table_itemName}>Ready</td>
-					<td className={st.table_item}>2</td>
-					<td className={st.table_item}>$40</td>
-					<td className={st.table_buttonGroup}>
-						<TableButton icon={<CartMinus />} />
-						<TableButton icon={<CartPlus />} />
-						<TableButton icon={<Close />} />
-					</td>
-				</tr>
-				<tr>
-					<td className={st.table_item}>1</td>
-					<td className={st.table_itemName}>
-						Production-Ready Microservices Production Ready Microservices
-					</td>
-					<td className={st.table_item}>2</td>
-					<td className={st.table_item}>$40</td>
-					<td className={st.table_buttonGroup}>
-						<TableButton icon={<CartMinus />} />
-						<TableButton icon={<CartPlus />} />
-						<TableButton icon={<Close />} />
-					</td>
-				</tr>
+				{
+					items.map((item, idx) => {
+						const {
+							id, name, count, total,
+						} = item;
+						return (
+							<tr key={id}>
+								<td className={st.table_item}>{idx + 1}</td>
+								<td className={st.table_itemName}>{name}</td>
+								<td className={st.table_item}>{count}</td>
+								<td className={st.table_item}>${total}</td>
+								<td className={st.table_buttonGroup}>
+									<TableButton
+										onClick={() => onDecrease(id)}
+										icon={<CartMinus />}
+									/>
+									<TableButton
+										onClick={() => onIncrease(id)}
+										icon={<CartPlus />}
+									/>
+									<TableButton
+										onClick={() => onDelete(id)}
+										icon={<Close />}
+									/>
+								</td>
+							</tr>
+						);
+					})
+				}
 			</tbody>
 			<tfoot>
 				<tr>
 					<td colSpan="4" className={st.table_totalText}>Total:</td>
-					<td className={st.table_totalAmount}>$222</td>
+					<td className={st.table_totalAmount}>${total}</td>
 				</tr>
 			</tfoot>
 		</table>
 	);
 
-// Table.propTypes = {
-// 	'book': PropTypes.shape({
-// 		'author': PropTypes.string,
-// 		'price': PropTypes.number,
-// 		'title': PropTypes.string,
-// 	}).isRequired,
-// };
+const mapStateToProps = ({ cartItems, orderTotal }) => ({
+	'items': cartItems,
+	'total': orderTotal,
+});
+
+const mapDispatchToProps = () => ({
+	onIncrease: (id) => {
+		console.log(`onIncrease ${id}`);
+	},
+	onDecrease: (id) => {
+		console.log(`onDecrease ${id}`);
+	},
+	onDelete: (id) => {
+		console.log(`onDelete ${id}`);
+	},
+});
+
+export const Table = connect(mapStateToProps, mapDispatchToProps)(BaseTable);
+
+BaseTable.propTypes = {
+	'items': PropTypes.arrayOf(PropTypes.object).isRequired,
+	'total': PropTypes.number.isRequired,
+	'onIncrease': PropTypes.func.isRequired,
+	'onDecrease': PropTypes.func.isRequired,
+	'onDelete': PropTypes.func.isRequired,
+};
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -8,24 +8,42 @@ const initialState = {
 	'books': [],
 	'loading': true,
 	'error': false,
+	'cartItems': [
+		{
+			'id': 1,
+			'name': 'book 1',
+			'count': 3,
+			'total': 150,
+		},
+		{
+			'id': 2,
+			'name': 'book 2',
+			'count': 4,
+			'total': 220,
+		},
+	],
+	'orderTotal': 220,
 };
 
 export const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_BOOKS_REQUEST:
 			return {
+				...state,
 				'books': [],
 				'loading': true,
 				'error': false,
 			};
 		case FETCH_BOOKS_SUCCESS:
 			return {
+				...state,
 				'books': action.payload,
 				'loading': false,
 				'error': false,
 			};
 		case FETCH_BOOKS_FAILURE:
 			return {
+				...state,
 				'books': [],
 				'loading': false,
 				'error': action.payload,
