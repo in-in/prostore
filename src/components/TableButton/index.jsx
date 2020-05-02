353972976c0b620da46e1d6548020233d2356709
in-in <dmin@tuta.io>
2020-05-02 18:56:23 +0300

    refactor(tablebutton): add onClick handler

--- src/components/TableButton/index.jsx
+++ src/components/TableButton/index.jsx
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 import st from './style.module.scss';
 
-export const TableButton = ({ icon }) =>
-// const {
-// 	// author, price, title,
-// } = book;
-
-	(
-
-		<button type="button" className={st.tableButton}>
-			{icon}
-		</button>
-
-	);
+export const TableButton = ({ icon, onClick }) => (
+	<button type="button" className={st.tableButton} onClick={onClick}>
+		{icon}
+	</button>
+);
 
 TableButton.propTypes = {
 	'icon': PropTypes.element.isRequired,
+	'onClick': PropTypes.func,
+};
+
+TableButton.defaultProps = {
+	'onClick': () => {},
 };
