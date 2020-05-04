bdd168a0527750298e46e43964562f9894be144c
in-in <dmin@tuta.io>
2020-05-04 11:42:23 +0300

    feat(redux): add logMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { reducer } from './reducers';
 
-export const store = createStore(reducer);
+const logMiddleware = ({ getState }) => (next) => (action) => {
+	// eslint-disable-next-line no-console
+	console.log(action.type, getState());
+	return next(action);
+};
+
+export const store = createStore(reducer, applyMiddleware(logMiddleware));
