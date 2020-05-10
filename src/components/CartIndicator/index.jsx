2188bf2b3529805284871bc5113e7c621a854ad1
in-in <dmin@tuta.io>
2020-05-10 18:38:37 +0300

    refactor(cartindicator): move total inside the cart

--- src/components/CartIndicator/index.jsx
+++ src/components/CartIndicator/index.jsx
@@ -8,18 +8,18 @@ export const BaseCartIndicator = ({ items }) => (
 	Boolean(items.length) && (
 		<Link href="/cart">
 			<a className={st.cartIndicator}>
-				<div className={st.cartIndicator_text}>
-					<span>
-						{
-							items.reduce((acc, i) => acc + i.count, 0)
-						} items
-					</span>
-					<span>(&#36;{
+				<div className={st.cartIndicator_wrapper}>
+					<Cart className={st.cartIndicator_icon} />
+					<span className={st.cartIndicator_total}>&#36;{
 						items.reduce((acc, i) => acc + i.total, 0)
-					})
+					}
 					</span>
 				</div>
-				<Cart className={st.cartIndicator_icon} />
+				<span>
+					{
+						items.reduce((acc, i) => acc + i.count, 0)
+					} items
+				</span>
 			</a>
 		</Link>
 	)
