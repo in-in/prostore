756b5900d10715889be8be9b6020d0e9882de69b
in-in <dmin@tuta.io>
2020-05-12 12:22:30 +0300

    refactor(errorindicator): create ErrorIndicator component

--- src/components/ErrorIndicator/index.jsx
+++ src/components/ErrorIndicator/index.jsx
@@ -1,7 +1,12 @@
+import Warning from '../../assets/icons/warning.svg';
 import st from './style.module.scss';
 
 export const ErrorIndicator = () => (
 	<div className={st.errorIndicator}>
-		ErrorIndicator
+		<Warning className={st.errorIndicator_icon} />
+		<h2>
+			Oops! Something went wrong!
+		</h2>
+		<p>Please reload the page and try again.</p>
 	</div>
 );
