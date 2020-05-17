44260325e2717041237da8462693c979ebe4b7eb
in-in <dmin@tuta.io>
2020-05-17 21:28:38 +0300

    feat(footer): add footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import st from './style.module.scss';
+
+export const Footer = () => (
+	<footer className={st.footer}>
+		<small>&copy; <a href="https://github.com/in-in">in-in</a>
+			, 2020
+		</small>
+	</footer>
+);
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,6 +6,7 @@ import { BookstoreService } from '@services/bookstore';
 import st from './style.module.scss';
 import { Header } from '~/components/Header';
 import { ErrorBoundary } from '~/components/ErrorBoundary';
+import { Footer } from '~/components/Footer';
 
 const bookstoreService = new BookstoreService();
 
@@ -18,6 +19,7 @@ export const Layout = ({ children }) => (
 				<main className={st.layout}>
 					{children}
 				</main>
+				<Footer />
 			</BookstoreServiceProvider>
 		</ErrorBoundary>
 	</Provider>
